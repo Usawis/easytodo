cd6bf1dcc897ed7a32169f09726c4d2df58ff2a3
Usawis <triplespy793@gmail.com>
2022-08-01 02:31:32 +0100

    second commit

--- src/firebase-script.js
+++ src/firebase-script.js
@@ -11,6 +11,10 @@ import {
     signOut,
 } from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js';
 
+import { FacebookAuthProvider } from "firebase/auth";
+
+const provider = new FacebookAuthProvider();
+
 // firestore imports
 import { 
     getFirestore,
@@ -286,3 +290,64 @@ if(window.location.href.includes('todos.html')) {
         console.log(err);
     });
 }
+
+provider.addScope('user_birthday');
+
+import { getAuth } from "firebase/auth";
+
+// const auth = getAuth();
+auth.languageCode = 'it';
+// To apply the default browser preference instead of explicitly setting it.
+// firebase.auth().useDeviceLanguage();
+
+import { getAuth, signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // The signed-in user info.
+    const user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+    const accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+
+    // ...
+  });
+
+  import { getAuth, signInWithRedirect } from "firebase/auth";
+
+const auth = getAuth();
+signInWithRedirect(auth, provider);
+
+import { getAuth, getRedirectResult, FacebookAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    const user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+    // ...
+  });
